999558e91fa17ada285cf1264d5cef26d5421f08
Patyas2 <patyassuceveanu@yahoo.com>
2025-07-20 01:59:27 +0200

    Rotating Robot

--- Assets/Scripts/RobotMovement.cs
+++ Assets/Scripts/RobotMovement.cs
@@ -5,14 +5,28 @@ public class RobotMovement : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
-  
+
+    [SerializeField] Sprite newSprite;
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         if (moveDirection.x == -1)
         {
-           
+            spriteRenderer.flipX = true;
         }
-        ProcessInput();
+        else if (moveDirection.x == 1)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveDirection.y == 1)
+        {
+            spriteRenderer.sprite = newSprite;
+        }
+            ProcessInput();
         
     }
     void FixedUpdate()
