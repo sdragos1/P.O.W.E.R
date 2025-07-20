7e32f6dd1b851297e743bae65c65f199e69833e9
Suceveanu Dragos <dragos.suceveanu@epitech.eu>
2025-07-20 14:07:56 +0200

    add line visual connection between nodes and robots

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,15 +1,32 @@
 using UnityEngine;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Robot : MonoBehaviour
 {
+    private LineRenderer _lineRenderer;
+    private readonly List<Vector3> _connectionPoints = new();
+    private const float _detectionRadius = 1.5f;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = 0;
+        _lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        _lineRenderer.startWidth = 0.05f;
+        _lineRenderer.endWidth = 0.05f;
+    }
+
     private void Update()
     {
         DetectNearbyPowerNodes();
     }
-    
+
     private void DetectNearbyPowerNodes()
     {
-        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, 1.5f);
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, _detectionRadius);
+
+        _connectionPoints.Clear();
 
         foreach (var hit in hits)
         {
@@ -17,11 +34,30 @@ public class Robot : MonoBehaviour
             if (node != null)
             {
                 node.ReceiveEnergyFrom(this);
+                _connectionPoints.Add(node.transform.position);
             }
         }
 
+        RenderConnections();
     }
-    
+
+    private void RenderConnections()
+    {
+        if (_connectionPoints.Count == 0)
+        {
+            _lineRenderer.positionCount = 0;
+            return;
+        }
+
+        _lineRenderer.positionCount = _connectionPoints.Count * 2;
+
+        for (int i = 0; i < _connectionPoints.Count; i++)
+        {
+            _lineRenderer.SetPosition(i * 2, transform.position);
+            _lineRenderer.SetPosition(i * 2 + 1, _connectionPoints[i]);
+        }
+    }
+
     public float GetPowerOutput()
     {
         return 10f;
