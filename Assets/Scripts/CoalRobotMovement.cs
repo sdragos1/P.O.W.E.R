1e832f4f69c97037037535a624754455c2e45410
Patyas2 <patyassuceveanu@yahoo.com>
2025-07-20 17:28:41 +0200

    Music and finished robot placement

--- Assets/Scripts/CoalRobotMovement.cs
+++ Assets/Scripts/CoalRobotMovement.cs
@@ -1,29 +1,26 @@
 using UnityEngine;
 
-public class RobotMovement : MonoBehaviour
+public class CoalRobotMovement : MonoBehaviour
 {
     public float moveSpeed;
-    public float tolerance;
-    public bool unchosen;
+    private float tolerance;
+    private bool unchosen;
     public Rigidbody2D rb;
-    private float Timer;
+    private Vector2 start;
     private Vector2 moveDirection;
+    private float changer;
 
     SpriteRenderer spriteRenderer;
-    
+   
     void Start()
     {
-        Timer = 60f;
-        
+       changer = 1;
         unchosen = true;
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
-        if (Timer < 60f)
-        {
-            Timer += 1;
-        }
+        
             
 
         if (moveDirection.x == -1)
@@ -39,11 +36,15 @@ public class RobotMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if (unchosen == false)
+        {
         Move();
-  
+        }
+
     }
     void ProcessInput()
     {
+
         if (unchosen == true)
         {
             if (Input.GetKeyDown("h"))
@@ -51,52 +52,89 @@ public class RobotMovement : MonoBehaviour
                 moveDirection.y = 0;
                 moveDirection.x = 1;
                 unchosen = false;
+                start = transform.position;
 
-                
             }
             else if (Input.GetKeyDown("v"))
             {
                 moveDirection.x = 0;
                 moveDirection.y = 1;
                 unchosen = false;
+                start = transform.position;
+
             }
         }
     }
     void resetMove()
     {
-        moveDirection *= -1;
-        Timer = 0f;
+        if (changer == 2)
+            {
+            moveDirection *= -1;
+            changer = 0;
+            }
+        start = transform.position;
+        float tempX = moveDirection.x;
+        moveDirection.x = moveDirection.y;
+        moveDirection.y = tempX;
+        changer += 1;
+
+    }
+    void explosion()
+    {
+        Destroy(gameObject);
     }
     void Move()
     {
         rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed,moveDirection.y * moveSpeed);
-        Vector2 Bar = transform.position;
+        Vector2 Current = transform.position;
         tolerance = 0.01f;
-        Debug.Log(Timer);
-        if (Timer == 60f)
+
+
+
+        if (Mathf.Abs(start.x + 3f - Current.x) < tolerance)
+          {
+            resetMove();
+          }
+       else if (Mathf.Abs(start.y + 3f - Current.y) < tolerance)
         {
-            if (Mathf.Abs(Bar.x - 19f) < tolerance)
-            {
-
-                resetMove();
-            }
-            else if (Mathf.Abs(Bar.y - 9f) < tolerance)
-            {
-
-                resetMove();
-            }
-            else if (Mathf.Abs(Bar.x) < tolerance)
-            {
-
-                resetMove();
-            }
-            else if (Mathf.Abs(Bar.y) < tolerance)
-            {
-
-                resetMove();
-            }
+            resetMove();
         }
-        
+        else if (Mathf.Abs(start.x - 3f - Current.x) < tolerance)
+        {
+            resetMove();
+        }
+        else if (Mathf.Abs(start.y - 3f - Current.y) < tolerance)
+        {
+            resetMove();
+        }
+        //Explosion
+        if (Mathf.Round(Current.x) == 20) 
+        {
+            
+            explosion();
+        }
+        else if (Mathf.Round(Current.y) == 10)
+        {
+            explosion();
+
+
+        }
+        else if (Mathf.Round(Current.x) == -1)
+        {
+            explosion();
+
+
+        }
+        else if (Mathf.Round(Current.y) == -1)
+        {
+            explosion();
+
+
+        }
+
+
+
+
     }
     
 }
