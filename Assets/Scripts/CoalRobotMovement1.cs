5e5b90a7cf4d92ded623b8fc4b87510ef94d8469
Patyas2 <patyassuceveanu@yahoo.com>
2025-07-20 16:42:05 +0200

    Fixed movement

--- Assets/Scripts/CoalRobotMovement1.cs
+++ Assets/Scripts/CoalRobotMovement1.cs
@@ -8,13 +8,13 @@ public class CoalRobotMovement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 start;
     private Vector2 moveDirection;
+    private float changer;
 
     SpriteRenderer spriteRenderer;
-    
+   
     void Start()
     {
-        
-
+       changer = 1;
         unchosen = true;
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -36,8 +36,11 @@ public class CoalRobotMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if (unchosen == false)
+        {
         Move();
-  
+        }
+
     }
     void ProcessInput()
     {
@@ -64,25 +67,36 @@ public class CoalRobotMovement : MonoBehaviour
     }
     void resetMove()
     {
+        if (changer == 2)
+            {
+            moveDirection *= -1;
+            changer = 0;
+            }
         start = transform.position;
         float tempX = moveDirection.x;
         moveDirection.x = moveDirection.y;
         moveDirection.y = tempX;
+        changer += 1;
+
+    }
+    void explosion()
+    {
+        Destroy(gameObject);
     }
     void Move()
     {
         rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed,moveDirection.y * moveSpeed);
         Vector2 Current = transform.position;
-        tolerance = 0.01f; 
+        tolerance = 0.01f;
+
+
+
         if (Mathf.Abs(start.x + 3f - Current.x) < tolerance)
           {
-            
             resetMove();
           }
        else if (Mathf.Abs(start.y + 3f - Current.y) < tolerance)
         {
-
-            moveDirection *= -1;
             resetMove();
         }
         else if (Mathf.Abs(start.x - 3f - Current.x) < tolerance)
@@ -91,12 +105,32 @@ public class CoalRobotMovement : MonoBehaviour
         }
         else if (Mathf.Abs(start.y - 3f - Current.y) < tolerance)
         {
-
-            moveDirection *= -1;
             resetMove();
         }
+        //Explosion
+        if (Mathf.Round(Current.x) == 20) 
+        {
+            
+            explosion();
+        }
+        else if (Mathf.Round(Current.y) == 10)
+        {
+            explosion();
 
 
+        }
+        else if (Mathf.Round(Current.x) == -1)
+        {
+            explosion();
+
+
+        }
+        else if (Mathf.Round(Current.y) == -1)
+        {
+            explosion();
+
+
+        }
 
 
 
